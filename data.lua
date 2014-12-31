1ef581096cd2c8b8a784d04bd81298d3b0d46ae4
AnodeCathode <shotorius_dt@hotmail.com>
2014-12-30 18:01:43 -0700

    Add support for Bob's Mods Synthetic Wood

--- data.lua
+++ data.lua
@@ -2,3 +2,6 @@ require("prototypes.entities")
 require("prototypes.items")
 require("prototypes.recipes")
 require("prototypes.projectiles")
+if data.raw.item["synthetic-wood"] then
+  require("prototypes.mod_recipes")
+end
---
+++ prototypes/mod_recipes.lua
@@ -0,0 +1,16 @@
+data:extend(
+	{
+		{
+			type = "recipe",
+			name = "landfill2by2synthetic",
+			enabled = "true",
+			ingredients =
+			{
+				{"stone", 100},
+				{"synthetic-wood", 25},
+				{"stone-wall", 10},
+						},
+			result = "landfill2by2"
+    }
+  }
+)	
